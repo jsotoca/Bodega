6a7c6769c7daefbaa68d0e42ca33ad8e78add527
jsotoca <jsotoca@unprg.edu.pe>
2021-05-07 01:54:25 -0500

    Se agrego User model y se configuro home controller para consumir api

--- Bodega/Controllers/HomeController.cs
+++ Bodega/Controllers/HomeController.cs
@@ -3,14 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using Bodega.Models;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace Bodega.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        public async Task<ActionResult> Index()
         {
-            return View();
+            string baseUrl = "https://jsonplaceholder.typicode.com/";
+            List<User> usuarios = new List<User>();
+
+            using (var cliente = new HttpClient())
+            {
+                cliente.BaseAddress = new Uri(baseUrl);
+                cliente.DefaultRequestHeaders.Clear();
+                cliente.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                var respuesta = await cliente.GetAsync("posts");
+                if (respuesta.IsSuccessStatusCode)
+                {
+                    var dataRespuesta = respuesta.Content.ReadAsStringAsync().Result;
+                    usuarios = JsonConvert.DeserializeObject<List<User>>(dataRespuesta);
+                }
+                return View(usuarios);
+            }
+
         }
 
         public ActionResult About()
---
+++ Bodega/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Bodega.Models
+{
+    public class User
+    {
+        public int userId { get; set; }
+        public string id { get; set; }
+        public string title { get; set; }
+        public string body { get; set; }
+    }
+}
